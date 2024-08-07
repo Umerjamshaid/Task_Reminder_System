2f874ca7517b826ed7b95c708ebe93158ce0825a
`UMERJAMSAID` <139961276+Umerjamshaid@users.noreply.github.com>
2024-08-07 14:37:52 +0500

    Update sheet.py

--- Midterm_project/sheet.py
+++ Midterm_project/sheet.py
@@ -5,7 +5,7 @@ import tkinter as tk
 from tkinter import messagebox 
 
 # Sheety API URL
-SHEETY_API_URL = 'sheety api'
+SHEETY_API_URL = 'https://api.sheety.co/347a860d7c6079a6e6250616420962dc/taskReminderSystemExpanded/taskReminderSystemExpandedCsv'
 
 # Function to fetch tasks from Google Sheet
 def fetch_tasks():
@@ -18,10 +18,10 @@ def fetch_tasks():
 
 # Function to send email reminder
 def send_email(to_address, task, due_date):
-    from_address = "your_email@gmail.com"
-    password = "your_password"
+    from_address = "umerjamshaid481@gmail.com"
+    password = "axrb pkxc kpqf liit"
     subject = f"Task Reminder: {task}"
-    body = f"Dear User,\n\nThis is a reminder for your task: {task}\nDue Date: {due_date}\n\nBest Regards,\nTask Reminder System"
+    body = f"Dear User,\n\nThis is a reminder for your task: {task}\nDue Date: {due_date}\n\nBest Regards,\nyour boss over here"
 
     message = f"Subject: {subject}\n\n{body}"
 
@@ -39,16 +39,18 @@ def run_task_reminders():
     for task in tasks:
         if task['DueDate'] == today:
             send_email(task['Email'], task['Task'], task['DueDate'])
-    messagebox.showinfo("Task Reminder", "Reminders sent for today's tasks!")
-
+            messagebox.showinfo("Task Reminder", "Reminders sent for today's tasks!")
+        else:
+            messagebox.showinfo("No tasks due today:")
 # making a GUI
 def create_gui():
     root = tk.Tk()
-#Size of the window
+
     root.geometry("400x300")
 
     guilable = tk.Label(root, text="Task Reminder System", font=("Arial", 16))
     guilable.pack()
+
 # creating a button
     button = tk.Button(root, text="Run Task Reminders", command=run_task_reminders, font=("Arial", 14), fg="gray", bg="black")
     button.pack()
